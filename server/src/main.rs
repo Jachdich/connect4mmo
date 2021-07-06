3f06fab4f03f568529d5089dfe0625b88cb8ca1e
Jachdich <jamesedkitching@gmail.com>
2021-07-06 13:43:04 +0100

    server and shit idfk

---
+++ server/src/main.rs
@@ -0,0 +1,143 @@
+use std::fmt;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::Write;
+use std::io::Read;
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+enum Piece {
+    None,
+    Red,
+    Yellow,
+}
+
+impl Piece {
+    fn from_u8(a: u8) -> Self {
+        match a {
+            0 => Piece::None,
+            1 => Piece::Red,
+            2 => Piece::Yellow,
+            _ => Piece::None,
+        }
+    }
+
+    fn to_u8(&self) -> u8 {
+        match self {
+            Piece::None => 0,
+            Piece::Red => 1,
+            Piece::Yellow => 2,
+        }
+    }
+}
+
+impl fmt::Display for Piece {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Piece::None => write!(f, " "),
+            Piece::Red => write!(f, "\x1b[101m \x1b[0m"),
+            Piece::Yellow => write!(f, "\x1b[103m \x1b[0m"),
+        }
+    }
+}
+struct Board {
+    data: [[Piece; 7]; 6],
+}
+
+impl Board {
+    fn new() -> Self {
+        let data: [[Piece; 7]; 6] = [[Piece::None; 7]; 6];
+        Board { data }
+    }
+    
+    fn from_buffer(buf: &[u8]) -> Self {
+        let mut data =  [[Piece::None; 7]; 6];
+        for x in 0..7 {
+            for y in 0..6 {
+                data[y][x] = Piece::from_u8(buf[y * 6 + x]);
+            }
+        }
+        Board { data }
+    }
+
+    fn to_buffer(&self) -> [u8; 7 * 6] {
+        let mut buf = [0 as u8; 7 * 6];
+        for x in 0..7 {
+            for y in 0..6 {
+               buf[y * 6 + x] = self.data[y][x].to_u8();
+            }
+        }
+        buf
+    }
+    
+    fn print_board(&self) {
+        println!("| 1 | 2 | 3 | 4 | 5 | 6 | 7 |");
+        println!("|---|---|---|---|---|---|---|");
+        for col in self.data.iter() {
+            for row in col.iter() {
+                print!("| {} ", row);
+            }
+            println!("|\n|---|---|---|---|---|---|---|");
+        }
+    }
+
+    fn place(&mut self, num: usize, colour: Piece) {
+        if self.data[0][num - 1] != Piece::None {
+            println!("This column is full!");
+            return;
+        }
+        let mut n = 0;
+        while n < 6 && self.data[n][num - 1] == Piece::None {
+            n += 1;
+        }
+        self.data[n - 1][num - 1] = colour;
+    }
+
+    fn check_win(&self) {
+        for x in 0..7 {
+            for y in 0..6 {
+                
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut board = Board::new();
+    let mut next_player = Piece::Red;
+    let listener = TcpListener::bind("127.0.0.1:42069").unwrap();
+    let mut conns: Vec<TcpStream> = Vec::new();
+    for stream in listener.incoming() {
+        conns.push(stream.unwrap());
+        if conns.len() == 2 { break; }
+    }
+    
+    loop {
+        let mut buf: [u8; 1] = [0; 1];
+        let idx = if next_player == Piece::Red { 0 } else { 1 };
+        let not_idx = if next_player == Piece::Red { 1 } else { 0 };
+        conns[idx].write(&[1 as u8; 1]);
+        conns[not_idx].write(&[2 as u8; 1]);
+        match conns[idx].read(&mut buf) {
+            Ok(size) => {
+                let pos = buf[0];
+                println!("Got pos {} from {}", pos, next_player);
+                if pos == 0 {
+                    //disconnected
+                    conns[idx].shutdown(Shutdown::Both).unwrap();
+                } else {
+                    board.place(pos as usize, next_player);
+                }
+            },
+            Err(_) => {
+                eprintln!("An error occurred, terminating connection with {}", conns[idx].peer_addr().unwrap());
+                conns[idx].shutdown(Shutdown::Both).unwrap();
+            }
+        }
+        for mut conn in &conns {
+            conn.write(&[0 as u8; 1]).unwrap();
+            conn.write(&board.to_buffer()).unwrap();
+        }
+
+        next_player = if next_player == Piece::Red { Piece::Yellow } else { Piece::Red };
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 use std::fmt;
-#[derive(Copy, Clone)]
+use std::net::{TcpStream};
+use std::io::{Read, Write, stdout};
+use std::thread;
+use termion::raw::IntoRawMode;
+use termion::input::TermRead;
+
+#[derive(Copy, Clone, PartialEq)]
 enum Piece {
     None,
     Red,
     Yellow,
 }
 
+impl Piece {
+    fn from_u8(a: u8) -> Self {
+        match a {
+            0 => Piece::None,
+            1 => Piece::Red,
+            2 => Piece::Yellow,
+            _ => Piece::None,
+        }
+    }
+
+}
+
 impl fmt::Display for Piece {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use termion::color::{Bg, LightRed, LightYellow, Reset};
         match self {
             Piece::None => write!(f, " "),
-            Piece::Red => write!(f, "x"),
-            Piece::Yellow => write!(f, "o"),
+            Piece::Red => write!(f, "{} {}", Bg(LightRed), Bg(Reset)),
+            Piece::Yellow => write!(f, "{} {}", Bg(LightYellow), Bg(Reset)),
         }
     }
 }
@@ -21,32 +40,132 @@ struct Board {
 
 impl Board {
     fn new() -> Self {
-        let data: [[Piece; 7]; 6] = [[Piece::None; 7]; 6];
+        Board { data: [[Piece::None; 7]; 6] }
+    }
+
+    fn update_from_buffer(&mut self, buf: &[u8]) {
+        let new_board = Board::from_buffer(buf);
+        self.data = new_board.data;
+    }
+    
+    fn from_buffer(buf: &[u8]) -> Self {
+        let mut data =  [[Piece::None; 7]; 6];
+        for x in 0..7 {
+            for y in 0..6 {
+                data[y][x] = Piece::from_u8(buf[y * 6 + x]);
+            }
+        }
         Board { data }
     }
-    fn print_board(&self) {
-        println!("| 1 | 2 | 3 | 4 | 5 | 6 | 7 |");
-        println!("|---|---|---|---|---|---|---|");
+    
+    fn print_board(&self, num: usize) {
+        for n in 1..8 {
+            print!("|{} {} {}", 
+                if n == num { termion::style::Invert.to_string() } else { termion::style::NoInvert.to_string() },
+                n,
+                termion::style::NoInvert,
+            );
+        }
+        println!("|\r");
+        for n in 1..8 {
+            print!("|{}---{}",
+                if n == num { termion::style::Invert.to_string() } else { termion::style::NoInvert.to_string() },
+                termion::style::NoInvert
+            );
+        }
+        println!("|\r");
         for col in self.data.iter() {
             for row in col.iter() {
                 print!("| {} ", row);
             }
-            println!("|\n|---|---|---|---|---|---|---|");
+            println!("|\r\n|---|---|---|---|---|---|---|\r");
+        }
+    }
+
+}
+
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+enum LocalMessage {
+    Event(termion::event::Event),
+    ChangeTurn(bool),
+}
+
+fn recv_thread(mut stream: TcpStream, tx: mpsc::Sender<LocalMessage>, board: Arc<Mutex<Board>>) {
+    loop {
+        let mut type_of_message = [0 as u8; 1];
+        stream.read(&mut type_of_message).unwrap();
+        if type_of_message[0] == 0 { //update board
+            let mut buf = [0 as u8; 7 * 6];
+            stream.read(&mut buf).unwrap();
+            let mut b = board.lock().unwrap();
+            (*b).update_from_buffer(&buf);
+        } else if type_of_message[0] == 1 { //our turn
+            tx.send(LocalMessage::ChangeTurn(true)).unwrap();
+        } else if type_of_message[0] == 2 { //not our turn lol
+            tx.send(LocalMessage::ChangeTurn(false)).unwrap();
         }
     }
 }
 
+fn run_events(tx: mpsc::Sender<LocalMessage>) {
+    let stdin = std::io::stdin();
+
+    for event in stdin.events() {
+        tx.send(LocalMessage::Event(event.as_ref().unwrap().clone())).unwrap();
+    }
+}
+
+use termion::event::Key;
+use termion::event::Event;
+
 fn main() {
-    let mut board = Board::new();
+    let mut stream = TcpStream::connect("127.0.0.1:42069").unwrap();
+    let other_stream = stream.try_clone().unwrap();
+    let mut screen = termion::screen::AlternateScreen::from(stdout()).into_raw_mode().unwrap();
+
+    let board = Arc::new(Mutex::new(Board::new()));
+    let other_board = Arc::clone(&board);
+    
+    let (tx, rx): (mpsc::Sender<LocalMessage>, mpsc::Receiver<LocalMessage>) = mpsc::channel();
+    let other_tx = tx.clone();
+    thread::spawn(move || {
+        recv_thread(other_stream, tx, other_board);
+    });
+    thread::spawn(move || {
+        run_events(other_tx);
+    });
+    let mut our_turn = false;
+    let mut num = 1;
     loop {
-        board.print_board();
-        let mut s = String::new();
-        std::io::stdin().read_line(&mut s).expect("what");
-        if let Some('\n') = s.chars().next_back() {
-            s.pop();
+        let message = rx.recv().unwrap();
+        if let LocalMessage::Event(e) = message {
+            match e {
+                Event::Key(Key::Char('\n')) => {
+                    if our_turn {
+                        let buf = [num as u8; 1];
+                        stream.write(&buf).unwrap();
+                    }
+                }
+                Event::Key(Key::Ctrl('c')) => break,
+                Event::Key(Key::Left)  => { if num > 1 && our_turn { num -= 1; } }
+                Event::Key(Key::Right) => { if num < 7 && our_turn { num += 1; } }
+                
+                _ => ()
+            }
+            
+        } else if let LocalMessage::ChangeTurn(turn) = message {
+            our_turn = turn;
         }
-        if let Some('\r') = s.chars().next_back() {
-            s.pop();
+        write!(screen, "{}", termion::clear::All).unwrap();
+        let b = board.lock().unwrap();
+        if our_turn {
+            (*b).print_board(num);
+        } else {
+            (*b).print_board(0);
         }
+        drop(b)
     }
 }
